05ea878a33bc15d4491d75b39a914f0247186e73
Georgi St. Lukanov <goshiii87@gmail.com>
2023-06-13 13:44:47 +0200

    add forms

--- spearfishing_app/accounts/urls.py
+++ spearfishing_app/accounts/urls.py
@@ -1,1 +1,25 @@
-urlpatterns = ()
+from django.urls import path, include
+
+from spearfishing_app.accounts.views import SingInView, SignInView, SignOutView, UserDetailsView, UserEditView, \
+    UserDeleteView, to_github
+
+# accounts/urls.py
+
+urlpatterns = (
+    path('register/', SingInView.as_view(), name='register'),
+    path('login/', SignInView.as_view(), name='login'),
+    path('logout/', SignOutView.as_view(), name='logout'),
+
+    path('profile/<int:pk>/', include(
+        [
+            path('', UserDetailsView.as_view(), name='profile details'),
+            path('edit/', UserEditView.as_view(), name='profile edit'),
+            path('delete/', UserDeleteView.as_view(), name='profile delete'),
+        ]
+    )),
+
+    path('to_github/', to_github, name='go to github'),
+)
+
+# need to add this to work with signals->
+# from .signals import *
--- spearfishing_app/accounts/views.py
+++ spearfishing_app/accounts/views.py
@@ -1,3 +1,61 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.contrib.auth import views as auth_views, get_user_model, login
+from django.urls import reverse_lazy
+from django.views import generic
 
-# Create your views here.
+from spearfishing_app.accounts.forms import UserCreateForm
+
+UserModel = get_user_model()
+
+
+class SignInView(auth_views.LoginView):
+    pass
+    # template_name = 'accounts/login-page.html'
+
+
+class SingInView(generic.CreateView):
+    pass
+    # template_name = 'accounts/register-page.html'
+    # form_class = UserCreateForm
+    # success_url = reverse_lazy('show index')
+    #
+    # # when new user is created,auto-login
+    # def post(self, request, *args, **kwargs):
+    #     response = super().post(request, *args, **kwargs)
+    #     login(request, self.object)
+    #     return response
+
+
+class SignOutView(auth_views.LogoutView):
+    next_page = reverse_lazy('index')
+
+
+class UserDetailsView(generic.DetailView):
+    pass
+    # model = UserModel
+    # template_name = 'accounts/profile-details-page.html'
+
+
+class UserEditView(generic.UpdateView):
+    pass
+    # template_name = 'accounts/profile-edit-page.html'
+    # model = UserModel
+    # fields = ('first_name', 'last_name', 'email', 'gender',)
+    #
+    # def get_success_url(self):
+    #     return reverse_lazy('profile details', kwargs={
+    #         'pk': self.request.user.pk,
+    #     })
+
+
+class UserDeleteView(generic.DeleteView):
+    pass
+    # template_name = 'accounts/profile-delete-page.html'
+    # model = UserModel
+    # success_url = reverse_lazy('show index')
+
+
+def to_github(request):
+    return HttpResponseRedirect(
+        "https://github.com/GeorgiLukanov87/PythonWeb-Django-Framework/tree/main/petstagram"
+    )
