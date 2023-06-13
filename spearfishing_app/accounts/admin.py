cfbcc147cbe6e42522b5fb6f7fc62c7583375118
Georgi St. Lukanov <goshiii87@gmail.com>
2023-06-13 16:07:55 +0200

    edit form added

--- spearfishing_app/accounts/admin.py
+++ spearfishing_app/accounts/admin.py
@@ -4,13 +4,15 @@ from django.contrib import admin
 
 from django.contrib.auth import admin as auth_admin, get_user_model
 
+from spearfishing_app.accounts.forms import UserEditForm, UserCreateForm
+
 UserModel = get_user_model()
 
 
 @admin.register(UserModel)
 class UserAdmin(auth_admin.UserAdmin):
-    # form = UserEditForm
-    # add_form = UserCreateForm
+    form = UserEditForm
+    add_form = UserCreateForm
 
     fieldsets = (
         (
