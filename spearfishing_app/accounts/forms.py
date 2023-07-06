0bbc036764042a6d992a5d93c789b4db11047418
Georgi St. Lukanov <goshiii87@gmail.com>
2023-07-06 21:00:12 +0200

    css

--- spearfishing_app/accounts/forms.py
+++ spearfishing_app/accounts/forms.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.contrib.auth import forms as auth_forms, get_user_model
 
 UserModel = get_user_model()
@@ -11,9 +12,35 @@ class UserEditForm(auth_forms.UserChangeForm):
 
 
 class UserCreateForm(auth_forms.UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['password1'].widget.attrs['placeholder'] = 'Enter Password...'
+        self.fields['password2'].widget.attrs['placeholder'] = 'Repeat Password...'
+
     class Meta:
         model = UserModel
         fields = ('username', 'email', 'profile_image_url',)
         field_classes = {
             'username': auth_forms.UsernameField,
+        }
+
+        widgets = {
+            'username': forms.TextInput(
+                attrs={
+                    'placeholder': 'Enter Username...',
+                },
+            ),
+
+            'email': forms.TextInput(
+                attrs={
+                    'placeholder': 'Enter Email...',
+                },
+            ),
+
+            'profile_image_url': forms.URLInput(
+                attrs={
+                    'placeholder': 'Link to Image...',
+                },
+            ),
+
         }
