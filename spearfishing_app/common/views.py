136c300de39803d4c247b93315122700fc9d550e
Georgi St. Lukanov <goshiii87@gmail.com>
2023-07-08 17:02:23 +0200

    .

--- spearfishing_app/common/views.py
+++ spearfishing_app/common/views.py
@@ -1,8 +1,9 @@
 import pyperclip
+
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
+
 from django.urls import reverse
 from django.views import generic
 
