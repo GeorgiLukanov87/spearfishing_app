652e0e5f8225fb17863ad6e1ede86a699d860c01
Georgi St. Lukanov <goshiii87@gmail.com>
2023-07-20 17:20:57 +0200

    edit equipment logic

--- spearfishing_app/equipment/urls.py
+++ spearfishing_app/equipment/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from spearfishing_app.equipment.views import add_equipment
+from spearfishing_app.equipment.views import add_equipment, EquipmentEditView
 
 urlpatterns = (
     path('add/', add_equipment, name='add-equipment'),
+    path('edit/<int:pk>/', EquipmentEditView.as_view(), name='edit-equipment'),
 )
--- spearfishing_app/equipment/views.py
+++ spearfishing_app/equipment/views.py
@@ -1,7 +1,10 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views import generic
 
 from spearfishing_app.equipment.forms import EquipmentAddForm
+from spearfishing_app.equipment.models import Equipment
 
 
 @login_required
@@ -19,3 +22,14 @@ def add_equipment(request):
     context = {'form': form, }
 
     return render(request, 'equipment/add-equipment.html', context)
+
+
+class EquipmentEditView(generic.UpdateView):
+    template_name = 'equipment/edit-equipment.html'
+    model = Equipment
+    fields = '__all__'
+
+    def get_success_url(self):
+        return reverse_lazy('profile details', kwargs={
+            'pk': self.request.user.pk,
+        })
