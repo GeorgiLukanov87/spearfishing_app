66bfc57942423b23f099d6fc5406b68082887a74
Georgi St. Lukanov <goshiii87@gmail.com>
2023-07-27 19:31:19 +0200

    403 error template

--- spearfishing_app/accounts/OwnerOrStaffMixin/required_mixin_owner_or_staff.py
+++ spearfishing_app/accounts/OwnerOrStaffMixin/required_mixin_owner_or_staff.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.mixins import UserPassesTestMixin
 from django.core.exceptions import PermissionDenied
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
 
 
@@ -9,7 +9,9 @@ class OwnerOrStaffRequiredMixin(UserPassesTestMixin):
         user = self.request.user
         return user.is_staff or user == self.get_object()
 
+    # def handle_no_permission(self):
+    #     if self.raise_exception:
+    #         raise PermissionDenied("You don't have permission to access this page.")
+    #     return redirect(reverse_lazy('index'))
     def handle_no_permission(self):
-        if self.raise_exception:
-            raise PermissionDenied("You don't have permission to access this page.")
-        return redirect(reverse_lazy('index'))
+        return render(self.request, 'accounts/permission_denied.html', status=403)
