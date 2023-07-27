38d8dca85f66dcaae2cf39e1b27288522ec42350
Georgi St. Lukanov <goshiii87@gmail.com>
2023-07-27 19:33:46 +0200

    Update required_mixin_owner_or_staff.py

--- spearfishing_app/accounts/OwnerOrStaffMixin/required_mixin_owner_or_staff.py
+++ spearfishing_app/accounts/OwnerOrStaffMixin/required_mixin_owner_or_staff.py
@@ -1,7 +1,5 @@
 from django.contrib.auth.mixins import UserPassesTestMixin
-from django.core.exceptions import PermissionDenied
-from django.shortcuts import redirect, render
-from django.urls import reverse_lazy
+from django.shortcuts import render
 
 
 class OwnerOrStaffRequiredMixin(UserPassesTestMixin):
@@ -9,9 +7,11 @@ class OwnerOrStaffRequiredMixin(UserPassesTestMixin):
         user = self.request.user
         return user.is_staff or user == self.get_object()
 
+    def handle_no_permission(self):
+        return render(self.request, 'accounts/permission_denied.html', status=403)
+
+    # Instant redirect without message
     # def handle_no_permission(self):
     #     if self.raise_exception:
     #         raise PermissionDenied("You don't have permission to access this page.")
     #     return redirect(reverse_lazy('index'))
-    def handle_no_permission(self):
-        return render(self.request, 'accounts/permission_denied.html', status=403)
