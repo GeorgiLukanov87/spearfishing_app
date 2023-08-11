76bc9c349677dc320891941f7c3452527a3e8e10
Georgi St. Lukanov <goshiii87@gmail.com>
2023-08-11 21:05:49 +0200

    equipment form add placeholders

--- spearfishing_app/equipment/forms.py
+++ spearfishing_app/equipment/forms.py
@@ -9,6 +9,32 @@ class EquipmentBaseForm(forms.ModelForm):
         model = Equipment
         exclude = ('owner',)
 
+        widgets = {
+            'gun_image': forms.URLInput(
+                attrs={
+                    'placeholder': 'Link to gun image...'
+                }
+            ),
+
+            'bands_image': forms.URLInput(
+                attrs={
+                    'placeholder': 'Link to bands image...'
+                }
+            ),
+
+            'fins_image': forms.URLInput(
+                attrs={
+                    'placeholder': 'Link to fins image...'
+                }
+            ),
+
+            'additional_info': forms.Textarea(
+                attrs={
+                    'placeholder': 'Add additional info here...'
+                }
+            ),
+        }
+
 
 class EquipmentAddForm(EquipmentBaseForm):
     pass
